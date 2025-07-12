2a0bab4bcc4a923725ee204b931ca24941c73d5e
elecdeer <37701077+elecdeer@users.noreply.github.com>
2025-07-12 14:56:36 +0900

    chore: 依存関係の修正など

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
+  entry: ["src/index.ts"],
+  format: ["esm"],
   dts: true,
   clean: true,
   sourcemap: true,
   splitting: false,
   minify: false,
-  target: 'node16',
-  external: ['eslint', '@eslint/css'],
+  target: "node22",
+  // external: ["eslint", "@eslint/css"],
 });
